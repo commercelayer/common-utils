7aa90b1891a229b1c9c0307091635106ded9bd6f
Giuseppe Ciotola <30926550+gciotola@users.noreply.github.com>
2025-02-17 12:26:21 +0100

    fix: remove `getAppsConfig`

--- packages/organization-config/src/index.ts
+++ packages/organization-config/src/index.ts
@@ -3,8 +3,3 @@ export {
   type MfeConfigs,
   type DefaultMfeConfig
 } from './getMfeConfig'
-export {
-  getAppsConfig,
-  type FullAppsConfig,
-  type AppsConfigWithDefault
-} from './getAppsConfig'
